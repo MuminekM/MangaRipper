6bb5c8cab88765ee244fd71d7372abee89b7d456
bhirsz <bartlomiej.hirsz@infotech.pl>
2017-08-04 15:24:51 +0200

    moved Title class to seperate class

---
+++ MangaRipper.Core/Models/Title.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaRipper.Core.Models
+{
+    public class Title
+    {
+        public string OriginalName { get; }
+        public string Url { get; }
+        public string Latest { get; }
+        public Title(string originalName, string url, string latest)
+        {
+            OriginalName = originalName;
+            Url = url;
+            Latest = latest;
+        }
+    }
+}
